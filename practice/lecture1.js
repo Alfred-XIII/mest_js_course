e308f1a6c2060472e9c36623ad3c936bfe3481ae
Jerry Y <jerry@local>
2016-10-23 18:36:12 +0000

    added homework for lecture1

---
+++ practice/lecture1.js
@@ -0,0 +1,43 @@
+/*
+ * change the following to make it valid string
+ */
+
+var schoolname = 'Mest";
+var answer = false;
+
+/*
+ * Build a person object with object literal notation, property must contains string, number and boolan
+ */
+
+/*
+ * Build the same person object with Object constructor function
+ */
+
+/*
+ * Access all the properties
+ */
+
+
+/*
+ *  Write a Greeting function
+ */
+
+/*
+ * Greeting function with parameter
+ */
+
+/*
+ * change the following expression to make them true
+ * (5 > 7)
+ * (7 === '7')
+ * (7 === 'seven')
+ * (false)
+ */
+
+/*
+ *  write the logic table for || and &&
+ */
+
+/*
+ * Use the for if condition write a weather function, inputs: temperature, isRainy
+ */
---
+++ practice/lecture2.js
@@ -0,0 +1,3 @@
+// how to assign this to a string variable - Jerry's friend said, "It's Tuesday\Oct 25."
+
+// make this long string to display as two lines: "JavaScript variables are containers for storing data values. Variables are named containers that you can place data in and then refer to the data by naming the container."
